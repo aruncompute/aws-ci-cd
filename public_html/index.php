793a589e4ef9a6b2631c3cb5b6c6ed6f80c4d91f
aruncompute <arun.compute@gmail.com>
2023-09-15 07:54:16 +0530

    Create index.php

---
+++ public_html/index.php
@@ -0,0 +1,4 @@
+<h2>Welcome to the Our World2</h2>
+<B>Server Address: </b> <?php echo $_SERVER['SERVER_ADDR'].':'.$_SERVER['SERVER_PORT'] ?><br>
+<b>Remote Address: </b> <?php echo $_SERVER['REMOTE_ADDR'].':'.$_SERVER['REMOTE_PORT'] ?><br>
+<b>Http Host: </b> <?php echo $_SERVER['HTTP_HOST'] ?>
