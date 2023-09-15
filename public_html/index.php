b3ec8af8b954b315b6e509b4e0cbff3188b44c3b
aruncompute <arun.compute@gmail.com>
2023-09-15 08:08:47 +0530

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,4 @@
-<h2>Welcome to the Our World2</h2>
+<h2>Welcome to the Our World3</h2>
 <B>Server Address: </b> <?php echo $_SERVER['SERVER_ADDR'].':'.$_SERVER['SERVER_PORT'] ?><br>
 <b>Remote Address: </b> <?php echo $_SERVER['REMOTE_ADDR'].':'.$_SERVER['REMOTE_PORT'] ?><br>
 <b>Http Host: </b> <?php echo $_SERVER['HTTP_HOST'] ?>
