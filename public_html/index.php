8ac3ba0a944e421746d8773e9cf71db09a3b7047
Arun Kumar Gupta <arun@qualsights.com>
2023-09-15 13:23:33 +0530

    API Update

--- public_html/index.php
+++ public_html/index.php
@@ -1,4 +1,4 @@
-<h2>Welcome to the Our World 5</h2>
+<h2>Welcome to the Our World 6</h2>
 <B>Server Address: </b> <?php echo $_SERVER['SERVER_ADDR'].':'.$_SERVER['SERVER_PORT'] ?><br>
 <b>Remote Address: </b> <?php echo $_SERVER['REMOTE_ADDR'].':'.$_SERVER['REMOTE_PORT'] ?><br>
 <b>Http Host: </b> <?php echo $_SERVER['HTTP_HOST'] ?>
